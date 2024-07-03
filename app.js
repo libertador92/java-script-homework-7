fbfbf51bf4e7adeccda3e745c11fd87f08bf18bd
libertador92 <miralizadenicat@gmail.com>
2024-07-03 06:22:40 -0400

    java script homework 7

---
+++ app.js
@@ -0,0 +1,97 @@
+let month = +prompt("Ayin nomresini reqemle daxil edin.")
+
+switch (month) {
+    case 1:
+        console.log("Yanvar");
+    break;
+    case 2:
+        console.log("Fevral");       
+    break;
+    case 3:
+        console.log("Mart");
+    break;
+    case 4:
+        console.log("Aprel");
+    break;
+    case 5:
+        console.log("May");
+    break;
+    case 6:
+        console.log("Iyun");
+    break;
+    case 7:
+        console.log("Iyul");
+    break;
+    case 8:
+        console.log("Avqust");
+    break;
+    case 9:
+        console.log("Sentyabr");
+    break;
+    case 10:
+        console.log("Oktyabr");
+    break;
+    case 11:
+        console.log("Noyabr");
+    break;
+    case 12:
+        console.log("Dekabr");
+    break;
+
+    default:
+        console.log("Tarix duzgun daxil edilmeyib.");
+        break;
+
+}
+
+// let date = prompt ("YYYY.MM.DD");
+// let il = date.split(".")[0];
+// let ay = date.split(".")[1];
+// let gun = date.split(".")[2];
+
+// if( ay == "01"){
+//     console.log( gun +  " " + "yanvar" + " " + il + "-ci il");
+// }
+// else if( ay == "02"){
+//     console.log( gun +  " " + "fevral" + " " + il + "-ci il");
+// }
+// else if( ay == "03"){
+//     console.log( gun +  " " + "mart" + " " + il + "-ci il");
+// }
+// else if( ay == "04"){
+//     console.log( gun +  " " + "aprel" + " " + il + "-ci il");
+// }
+// else if( ay == "05"){
+//     console.log( gun +  " " + "may" + " " + il + "-ci il");
+// }
+// else if( ay == "06"){
+//     console.log( gun +  " " + "iyun" + " " + il + "-ci il");
+// }
+// else if( ay == "07"){
+//     console.log( gun +  " " + "iyul" + " " + il + "-ci il");
+// }
+// else if( ay == "08"){
+//     console.log( gun +  " " + "avqust" + " " + il + "-ci il");
+// }
+// else if( ay == "09"){
+//     console.log( gun +  " " + "sentyabr" + " " + il + "-ci il");
+// }
+// else if( ay == "10"){
+//     console.log( gun +  " " + "oktyabr" + " " + il + "-ci il");
+// }
+// else if( ay == "11"){
+//     console.log( gun +  " " + "noyabr" + " " + il + "-ci il");
+// }
+// else if( ay == "12"){
+//     console.log( gun +  " " + "dekabr" + " " + il + "-ci il");
+// }
+// else{
+//     console.log("Tarix duzgun daxil edilmeyib.");
+// }
+
+
+
+
+
+
+
